b7d6f68d1b636f03ed9f890ca92aa0a9d80fbe43
Gabriel Teodoro <teodorogabriel367@gmail.com>
2023-02-10 22:33:53 -0300

    Versão 0.2

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+//VARIAVEIS
+
 var salariobruto = Number(document.getElementById("salario").value)
-var dependente = Number(document.getElementById("dependente").value)
+var dependencia = Number(document.getElementById("dependente").value)
+const conta = document.getElementById("conta")
 var inss
 var irrf
 var baseIR
 var vt
-var desconto
 var salarioliquido
+var enviar
 
 //CALCULO DE VT
 
-vt = salariobruto*0.06
+function vt() {
+    vt = salariobruto*0.06
+}
 
 // CALCULO DE INSS
 
@@ -19,7 +24,7 @@ if(salariobruto<=1212.00){
 else if(2427.35<=salariobruto>=1212.01){
     inss = salariobruto*0.09
 }
-else if(3641.03<=salariobruto>=2427.35){
+else if(3641.03<=salariobruto>=2427.36){
     inss = salariobruto*0.12
 }
 else{
@@ -28,7 +33,7 @@ else{
 
 //CALCULO DE IRRF
 
-var baseIR = salariobruto - inss - 189.59*dependente
+var baseIR = salariobruto - inss - 189.59*dependencia
 
 if(2826.65<=salariobruto>=1903.99){
     irrf = baseIR*0.075-142,80
@@ -43,3 +48,22 @@ else{
     irrf = baseIR*0.275-142,80
 }
 
+//CALCULO DE SALARIO LIQUIDO
+
+salarioliquido = salariobruto - inss - irrf - vt
+
+//FUNÇÃO ENVIAR
+
+function enviar(){
+    conta.innerHTML = `
+        <text>RESULTADO</text>
+        <table>
+            <tr> <td>Salário bruto</td> <td>${salariobruto}</td> </tr>
+            <tr> <td>INSS</td> <td>${inss}</td> </tr>
+            <tr> <td>IRRF</td> <td>${irrf}</td> </tr>
+            <tr> <td>Vale Transporte</td> <td>${vt}</td> </tr>
+            <tr> <td>Salário Líquido</td> <td>${salarioliquido}</td> </tr>
+        </table>
+        <button>Resetar</button>
+        `
+}
